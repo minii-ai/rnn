gradients r correct

rnn.py
@@ -4,11 +4,8 @@ import pickle
 
 def softmax(z, t: float = 1.0):
     """Softmax w/ temperature t"""
-    logits = z / t
-    max_logit = np.max(logits)
-    exp_logits = np.exp(logits - max_logit)
-    probs = exp_logits / np.sum(exp_logits)
-    return probs
+    z_exp = np.exp(z / t)
+    return z_exp / np.sum(z_exp)
 
 
 class RNN:
@@ -98,26 +95,26 @@ class RNN:
         the loss, gradients (dWxh, dWhh, dWhy, dbh, dby), and final hidden state
         """
         assert len(inputs) == len(targets)
-        hprev = np.zeros((1, self.hidden_size)) if hprev is not None else np.copy(hprev)
         xs, hs, ps = {}, {}, {}  # keep track of x, hidden states, and output probs
-        hs[-1] = hprev  # store initial hidden state
+        hs[-1] = (
+            hprev if hprev is not None else np.zeros((1, self.hidden_size))
+        )  # store initial hidden state
+
         inputs, targets = [self.char_to_idx[char] for char in inputs], [
             self.char_to_idx[char] for char in targets
         ]
         loss = 0
 
-        # compute loss at each timestep
+        # forward pass
         for t in range(len(inputs)):
             x = np.zeros((1, self.vocab_size))
-            x[0, [inputs[t]]] = 1  # one hot encoding of char
-            probs, h = self(x, hs[t - 1])  # x, hprev -> rnn -> probs, hnext
+            x[0, inputs[t]] = 1  # one hot encoding
+            p, h = self(x, hs[t - 1])  # rnn
 
-            xs[t] = x  # store xs, hs, and probs, we'll use them during backprop
+            xs[t] = x  # store x, hidden state, probs (we'll need them for backprop)
             hs[t] = h
-            ps[t] = probs
-
-            # cross entropy loss, nll of the predicted prob for the target char
-            loss += -np.log(probs[0, targets[t]])
+            ps[t] = p
+            loss += -np.log(ps[t][0, targets[t]])
 
         # gradient of loss w.r.t weights and biases
         dWxh, dWhh, dWhy = (

train.py
@@ -38,7 +38,8 @@ def build_vocab(data: str) -> str:
 
 def clip_gradients(gradients):
     for gradient in gradients:
-        np.clip(gradient, -1, 1, out=gradient)  # clip gradient in-place
+        # np.clip(gradient, -1, 1, out=gradient)  # clip gradient in-place
+        np.clip(gradient, -5, 5, out=gradient)  # clip gradient in-place
 
 
 def train_loop(
@@ -74,12 +75,12 @@ def train_loop(
     smooth_loss = -np.log(1.0 / rnn.vocab_size) * seq_length
 
     with tqdm(total=iters, position=0) as pbar:
-        for iter in range(1000000000000000000):
+        for iter in range(iters):
             if i >= len(data) - 1:
                 i = 0  # reset to start of data
                 h = np.zeros((1, rnn.hidden_size))  # reset hidden state
 
-            batch = data[i : i + seq_length + 1]
+            batch = data[i : i + seq_length]
             inputs, targets = batch[:-1], batch[1:]  # prepare input, target for loss
 
             # compute loss and gradients
@@ -94,16 +95,18 @@ def train_loop(
                 mem += gradient**2
                 weight -= lr * gradient / np.sqrt(mem + 1e-8)
 
-            if (iter + 1) % val_steps == 0:  # validation step
-                tqdm.write(f"== Iter {iter} ==")
+            if (
+                (iter + 1) % val_steps == 0 or iter == iters - 1 or iter == 0
+            ):  # validation step
+                tqdm.write(f"== Iter {iter + 1} ==")
                 tqdm.write(f"loss = {smooth_loss}")
                 sample = rnn.sample(val_c, val_n, val_t)
                 tqdm.write(sample)
 
             i += seq_length  # move to next batch
 
-            # pbar.set_postfix(loss=loss)
-            # pbar.update(1)
+            pbar.set_postfix(loss=loss)
+            pbar.update(1)
 
     print(f"[INFO] Final Loss = {loss}")
     print("[INFO] Sample")