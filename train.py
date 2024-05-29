1ef132e3c8fc7bfe1707d60318ba686d8566612b
Eric Chen <deeplearningericchen@gmail.com>
2024-05-28 18:53:23 -0700

    almost ready

--- train.py
+++ train.py
@@ -1,18 +1,19 @@
-import argparse
 import numpy as np
 from rnn import RNN
+import argparse
+import re
+import random
 from tqdm import tqdm
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data", "-d", type=str, default="./data/stevejobs.txt")
+    parser.add_argument("--data", "-d", type=str, default="./data/stevejobs_short.txt")
     parser.add_argument("--hidden_size", "-hs", type=int, default=64)
     parser.add_argument("--iters", "-i", type=int, default=1000)
     parser.add_argument("--lr", type=float, default=1e-1)
     parser.add_argument("--seq_length", "-s", type=int, default=25)
-    parser.add_argument("--save_path", "-sp", required=True)
-    parser.add_argument("--val_n", "-vn", type=int, default=50)
+    parser.add_argument("--save_path", "-sp")
     parser.add_argument("--val_steps", "-vs", type=int, default=100)
     parser.add_argument("--val_c", "-vc", type=str, default="h")
     parser.add_argument("--val_t", "-vt", type=float, default=0.5)
@@ -43,17 +44,16 @@ def clip_gradients(gradients):
 
 def train_loop(
     rnn: RNN,
-    data: str,
+    dataset: list[str],
     iters: int,
     lr: float,
     seq_length: int,
-    val_n: int,
     val_steps: int,
     val_c: str,
     val_t: float,
 ):
     print("[INFO] Training...")
-    print(f"[INFO] data_size = {len(data)}")
+    print(f"[INFO] data_size = {len(dataset)}")
     print(f"[INFO] num_params = {rnn.num_params}")
     print(f"[INFO] vocab_size = {rnn.vocab_size}")
     print(f"[INFO] hidden_size = {rnn.hidden_size}")
@@ -61,79 +61,79 @@ def train_loop(
     print(f"[INFO] iters = {iters}")
     print(f"[INFO] seq_length = {seq_length}")
 
-    i = 0
-    h = np.zeros((1, rnn.hidden_size))
-
-    # for adagrad (https://gist.github.com/karpathy/d4dee566867f8291f086)
+    # memory variables for Adagrad
     mWxh, mWhh, mWhy = (
         np.zeros_like(rnn.Wxh),
         np.zeros_like(rnn.Whh),
         np.zeros_like(rnn.Why),
     )
     mbh, mby = np.zeros_like(rnn.bh), np.zeros_like(rnn.by)
-    smooth_loss = -np.log(1.0 / rnn.vocab_size) * seq_length
 
-    with tqdm(total=iters, position=0) as pbar:
-        for iter in range(iters):
-            if i >= len(data) - 1:
-                i = 0  # reset to start of data
-                h = np.zeros((1, rnn.hidden_size))  # reset hidden state
+    n = 0
+    pbar = tqdm(total=iters, position=0)
+    smooth_loss = -np.log(1.0 / rnn.vocab_size) * seq_length  # loss at iteration 0
 
-            batch = data[i : i + seq_length]
-            inputs, targets = batch[:-1], batch[1:]  # prepare input, target for loss
-            inputs, targets = rnn.encode(inputs), rnn.encode(targets)
+    while True:
+        batches = random.sample(dataset, len(dataset))
+        for minibatch in batches:
+            hprev = np.zeros((1, rnn.hidden_size))  # reset RNN memory
+            minibatch_idxs = rnn.encode(minibatch)
 
-            # compute loss and gradients
-            loss, gradients, h = rnn.loss(inputs, targets, h)
-            smooth_loss = smooth_loss * 0.999 + loss * 0.001
-            clip_gradients(gradients)  # gradient clipping for training stability
+            for p in range(0, len(minibatch), seq_length):
+                batch = minibatch_idxs[p : p + seq_length + 1]
+                inputs, targets = batch[:-1], batch[1:]
 
-            # adagrad gradient descent
-            for weight, gradient, mem in zip(
-                rnn.weights, gradients, [mWxh, mWhh, mWhy, mbh, mby]
-            ):
-                mem += gradient**2
-                weight -= lr * gradient / np.sqrt(mem + 1e-8)
+                # compute loss and clip gradients
+                loss, gradients, hprev = rnn.loss(inputs, targets, hprev)
+                smooth_loss = smooth_loss * 0.999 + loss * 0.001
+                clip_gradients(gradients)
 
-            if (
-                (iter + 1) % val_steps == 0 or iter == iters - 1 or iter == 0
-            ):  # validation step
-                tqdm.write(f"== Iter {iter + 1} ==")
-                tqdm.write(f"loss = {smooth_loss}")
-                sample = rnn.sample(val_c, val_n, val_t)
-                tqdm.write(sample)
+                # sample
+                if (n + 1) % val_steps == 0 or n == 0 or n == iters - 1:
+                    tqdm.write(f"iter: {n + 1}, loss: {smooth_loss}")
+                    idxes = rnn.sample(rnn.char_to_idx[val_c], val_t)
+                    txt = rnn.decode(idxes)
+                    tqdm.write(f"----\n{txt}\n----")
+                    tqdm.write("")
 
-            i += seq_length  # move to next batch
+                # adagrad gradient descent
+                for param, dparam, mem in zip(
+                    rnn.weights,
+                    gradients,
+                    [mWxh, mWhh, mWhy, mbh, mby],
+                ):
+                    mem += dparam**2
+                    param += -lr * dparam / np.sqrt(mem + 1e-8)  # adagrad update
 
-            pbar.set_postfix(loss=loss)
-            pbar.update(1)
+                n += 1
+                pbar.update(1)
 
-    print(f"[INFO] Final Loss = {loss}")
-    print("[INFO] Sample")
-    print(rnn.sample(val_c, val_n))
+                if n >= iters:
+                    break
+            if n >= iters:
+                break
+        if n >= iters:
+            break
+
     print("[INFO] Training complete!")
 
 
 def main(args):
-    # np.random.seed(0)
-    data = read_file(args.data)  # read data from txt file
+    data = read_file(args.data)  # read txt file
+    dataset = re.split(r"\n\s*\n", data)  # split data into paragraphs
     vocab = build_vocab(data)  # build vocab of unique chars
-    rnn = RNN(hidden_size=args.hidden_size, vocab=vocab)  # init rnn
+    rnn = RNN(args.hidden_size, vocab)
 
     train_loop(
         rnn=rnn,
-        data=data,
+        dataset=dataset,
         iters=args.iters,
         lr=args.lr,
         seq_length=args.seq_length,
-        val_n=args.val_n,
         val_steps=args.val_steps,
         val_c=args.val_c,
         val_t=args.val_t,
     )
-
-    print("[INFO] Saving model...")
-    rnn.save(args.save_path)
 
 
 if __name__ == "__main__":
